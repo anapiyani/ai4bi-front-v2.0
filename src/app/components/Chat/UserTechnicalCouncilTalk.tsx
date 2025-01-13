27cf3b48b2b3dfd2ac915957bf8c45ef44694bfa
Anapiya Nurkeldi <anapiyanurkeldi@gmail.com>
2025-01-13 15:54:03 +0500

    fixed console errors for svg's

--- src/app/components/Chat/UserTechnicalCouncilTalk.tsx
+++ src/app/components/Chat/UserTechnicalCouncilTalk.tsx
@@ -56,9 +56,9 @@ const UserTechnicalCouncilTalk = ({
 						<DropdownMenu>
 							<DropdownMenuTrigger>
 							<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path d="M16 12.25V11.75" stroke="#0F172A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-								<path d="M12 12.25V11.75" stroke="#0F172A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-								<path d="M8 12.25V11.75" stroke="#0F172A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M16 12.25V11.75" stroke="#0F172A" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+								<path d="M12 12.25V11.75" stroke="#0F172A" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+								<path d="M8 12.25V11.75" stroke="#0F172A" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
 							</svg>
 							</DropdownMenuTrigger>
 							<DropdownMenuContent className='bg-primary-foreground'>
