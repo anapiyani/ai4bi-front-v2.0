9303675c06700e8883227ae0b252b900eeba76aa
Anapiya Nurkeldi <anapiyanurkeldi@gmail.com>
2025-01-23 18:54:48 +0500

    fix

--- src/app/components/Chat/ChatMenu/AuctionChatMenu.tsx
+++ src/app/components/Chat/ChatMenu/AuctionChatMenu.tsx
@@ -2,8 +2,8 @@
 
 const AuctionChatMenu = ({t}: {t: any}) => {
 	return (
-		<div className='flex flex-col gap-2'>
-			<p className='text-sm text-muted-foreground'>GET A LIFE I'M TOO LAZY TO DO THIS</p>
+		<div className="flex flex-col gap-2">
+			<p className="text-sm text-muted-foreground">GET A LIFE IM TOO LAZY TO DO THIS</p>
 		</div>
 	)
 }
