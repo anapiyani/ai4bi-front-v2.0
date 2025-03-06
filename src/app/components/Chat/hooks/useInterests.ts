a6028d45229cf5af1e59f30c7432bdfbd89aa5d5
Anapiya Nurkeldi <anapiyanurkeldi@gmail.com>
2025-03-06 19:06:03 +0500

    fix: interests add

--- src/app/components/Chat/hooks/useInterests.ts
+++ src/app/components/Chat/hooks/useInterests.ts
@@ -2,8 +2,8 @@ import { get, post } from '@/src/app/api/service/api'
 import { createQueryKeys } from '@/src/app/api/service/queryKeys'
 import { InterestsResponse } from '@/src/app/types/types'
 import { useMutation, useQuery } from '@tanstack/react-query'
+import { useTranslations } from 'next-intl'
 import toast from 'react-hot-toast'
-
 
 const interestsKey = createQueryKeys("interests");
 const getInterests = () => get<InterestsResponse[]>('/interests/');
@@ -23,15 +23,16 @@ const addInterests = (conditions: {
 }) => post<{detail: string}>(`/interests/user/${conditions.user_id}?interest_ids=${conditions.interests_id.join('&interest_ids=')}`);
 
 export const useAddInterests = () => {
+	const t = useTranslations("dashboard");
 	return useMutation({
 		mutationFn: addInterests,
 		onSuccess: (data) => {
-			toast.success(data.detail, {
+			toast.success(t('interest_added_successfully'), {
 				duration: 3000,
 			})
 		},
 		onError: (error) => {
-			toast.error(error.message, {
+			toast.error(t('interest_added_error'), {
 				duration: 3000,
 			})
 		}
